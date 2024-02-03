10f9807d3d47012c5c4d7973272f445227b0be08
Kruti <21kruti@gmail.com>
2024-02-03 18:29:18 +0530

    new file:   lib/wordle/views/wordle_screen.dart

---
+++ lib/wordle/views/wordle_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WordleScreen extends StatefulWidget{
+  const WordleScreen({ Key? key}) : super(key: key);
+
+  @override
+  _WordleScreenState createState() => _WordleScreenState();
+}
+
+class _WordleScreenState extends State<WordleScreen> {
+  @override
+  Widget build(BuildContext context){
+    return Container (
+
+    );
+  }
+}
