6bb6f0f57805502cef5909d64ec9d9deb6c579bd
Kruti <21kruti@gmail.com>
2024-02-03 18:12:39 +0530

    modified:   lib/wordle/models/letter_model.dart

--- lib/wordle/models/letter_model.dart
+++ lib/wordle/models/letter_model.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_wordle_clone/app/app_colors.dart';
 
 enum LetterStatus {initial, notInWord, inWord, correct}
 
@@ -13,4 +14,40 @@ class Letter extends Equatable {
   final String val;
 
   final LetterStatus status;
+
+  Color get backgroundColor {
+    switch(status){
+      case LetterStatus.initial:
+        return Colors.transparent;
+      case LetterStatus.notInWord:
+        return notInWordColor;
+      case LetterStatus.inWord:
+        return inWordColor;
+      case LetterStatus.correct:
+        return correctColor;
+    }
+  }
+
+  Color get borderColor {
+    switch (status){
+      case LetterStatus.initial:
+        return Colors.grey;
+        default:
+          return Colors.transparent;
+    }
+  }
+
+  Letter copyWith({
+    String? val,
+    LetterStatus? status,
+  }) {
+    return Letter(
+      val: val ?? this.val,
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object?> get props => [val, status];
 }
+
