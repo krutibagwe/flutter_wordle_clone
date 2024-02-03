1ba0c99c45a6c8b2bb91484084b58d8a7c461ba4
Kruti <21kruti@gmail.com>
2024-02-03 18:21:08 +0530

    new file:   lib/wordle/models/word_model.dart

---
+++ lib/wordle/models/word_model.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_wordle_clone/wordle/wordle.dart';
+
+class Word extends Equatable{
+  const Word({required this.letters});
+
+  factory Word.fromString(String word) =>
+    Word(letters: word.split('').map((e)=> Letter(val: e)).toList());
+
+    final List<Letter> letters;
+
+    String get wordString => letters.map((e)=>e.val).join();
+
+    void addLetter(String val){
+      final currentIndex = letters.indexWhere((e) => e.val.isEmpty);
+      if (currentIndex != -1){
+        letters[currentIndex] = Letter(val: val);
+      }
+    }
+    
+}
